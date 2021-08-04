b73cf675bc569e9c176ad29f2ab3e2f53940e96c
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-04 21:22:03 +0600

    inch-to-feet

---
+++ Inch-to-Feet.js
@@ -0,0 +1,10 @@
+// var inches = 123;
+// var feet = inches/12;
+// console.log(feet);
+function inchToFeet(inches){
+    var feet = inches/12;
+    return feet;
+}
+var myinches = 18;
+var feet = inchToFeet(myinches);
+console.log('My inches in feet ', feet);
