25cf831d88c1b506a1b000a5b943aaabf907384c
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-04 21:25:59 +0600

    miles to kilomiter

---
+++ miles-to-kilometer.js
@@ -0,0 +1,6 @@
+function milesToKilometer(miles){
+    var kilometer = miles * 1.60934;
+    return kilometer;
+}
+var marathon = milesToKilometer(26.2);
+console.log('Marathon in kilomiter: ', marathon);
